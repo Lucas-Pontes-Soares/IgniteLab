0fd460923fa9072e0695e8824f723b093194a987
Lucas <lucas.soares177@etec.sp.gov.br>
2022-07-06 12:52:52 -0300

    Adicionando arquivos da segunda aulas

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,9 @@
-
-import { gql } from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
-import { useQuery } from "@apollo/client"
-
-const GET_LESSONS_QUERRY = gql`
-  query {
-      lessons {
-        id
-        title
-      }
-    }
-`
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Event } from "./pages/Event"
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERRY)
-
-  //data?.lessons, percorrer lesson (map) e a cada lesson retornar ...
-  // ? so procurar pelas lessons caso o data n seja vazio
   return (
-   <ul> 
-    {data?.lessons.map(lesson => {
-      return <li key={lesson.id}>{lesson.title}</li>
-    })}
-   </ul>
-  )
+    <Event />
+  ) 
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,36 @@ module.exports = {
     './src/**/*.tsx'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: 'Roboto, sans-serif',
+      },
+      colors: {
+        green: {
+          300: '#88B37E',
+          500: '#88875F',
+          700: '#815F43',
+        },
+        blue: {
+          500: '#81D8F7',
+        },
+        orange: {
+          500: '$F75A68',
+        },
+        red: {
+          500: '#F75A68',
+        },
+        gray: {
+          100: '#E1E1E6',
+          200: '#C4C4CC',
+          300: '#8D8D99',
+          500: '#323238',
+          600: '#29292E',
+          700: '#121214',
+          900: '#09090A'
+        }
+      }
+    },
   },
   plugins: [],
 }
